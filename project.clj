11cd6d95df50ac0fdb2b8062f3ee621e07e5db51
Andrew <andrew.s.lai5@gmail.com>
2021-03-04 20:32:15 -0600

    Update project.clj description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject reframe-figwheel/lein-template "0.1.0"
-  :description "FIXME: write description"
+  :description "Reframe SPA built with figwheel-main template"
   :url "https://github.com/ALai57/reframe-figwheel-template"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
